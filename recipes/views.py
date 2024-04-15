bde6814253f2f328e3dee76101b7569ab3c6f3b6
ymtec90 <ymfstec@gmail.com>
2024-04-15 18:51:34 -0300

    created recipes app

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "recipes/home.html")
+
+
+def contato(request):
+    return render(request, "recipes/contato.html")
+
+
+def sobre(request):
+    return render(request, "recipes/sobre.html")
